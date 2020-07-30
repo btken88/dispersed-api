bc692b71f8b0046a3eff9cd65c90017274ebdabe
Bryce Kennedy <btken88@gmail.com>
2020-07-30 11:44:10 -0600

    Create route handling for user creation (/register) and login (/login) along with validations and connections to the database

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const app = express()
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/.env' })
 
 connectDB();
 
 const Favorite = require('./models/Favorite')
+const User = require('./models/User')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -39,3 +43,79 @@ app.post('/favorites', (req, res) => {
       res.status(201).json(data)
     }).catch(console.error)
 })
+
+app.post('/register',
+  [
+    check('username', 'Please enter a valid username').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({ min: 6 })
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, email, password } = req.body;
+    try {
+      let user = await User.findOne({ username });
+      if (user) {
+        return res.status(400).json({
+          msg: "User Already Exists"
+        });
+      }
+
+      user = new User({ username, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = { user_id: user.id }
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Error in Saving");
+    }
+  });
+
+app.post('/login',
+  [
+    check("username", "Please enter a valid username").isLength({ min: 1 }),
+    check("password", "Please enter a valid password").isLength({ min: 6 })
+  ], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    console.log(req.body)
+    const { username, password } = req.body
+    try {
+      let user = await User.findOne({ username })
+      if (!user) {
+        return res.status(400).json({ message: "Incorrect username or password" })
+      }
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res.status(400).json({ message: "Incorrect username or password" })
+      }
+      const payload = { user_id: user.id }
+      jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+        if (err) throw err;
+        res.status(200).json({ token })
+      })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ message: "Server Error" })
+    }
+  })
