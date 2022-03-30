9e3b0bc0af85f77ff73d130f139997f6d0a2c714
Bryce Kennedy <bryce@brycekennedy.net>
2022-03-29 21:13:13 -0600

    Updates cors and user model

--- server.js
+++ server.js
@@ -1,73 +1,81 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const { check, validationResult } = require('express-validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
-const connectDB = require('./config/db')
-const app = express()
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const app = express();
 
-dotenv.config({ path: './config/.env' })
+dotenv.config({ path: "./config/.env" });
 
 connectDB();
 
-const Favorite = require('./models/Favorite')
-const User = require('./models/User')
-const Bugreport = require('./models/Bugreport')
+const Favorite = require("./models/Favorite");
+const User = require("./models/User");
+const Bugreport = require("./models/Bugreport");
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(bodyParser.json());
+app.use(cors({ origin: "https://dispersed.app" }));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
 const server = app.listen(5000, () => {
   console.log('listening on 5000')
 })
 
 // Handle promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`)
-  server.close(() => process.exit(1))
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
 
-app.get('/favorites', authorizeUser, (req, res) => {
-  Favorite.find({ user_id: req.user_id })
-    .then(data => {
-      res.status(200).json(data)
-    })
-})
+app.get("/favorites", authorizeUser, (req, res) => {
+  Favorite.find({ user_id: req.user_id }).then((data) => {
+    res.status(200).json(data);
+  });
+});
 
-app.post('/favorites', authorizeUser, (req, res) => {
-  const favorite = { ...req.body, user_id: req.user_id }
+app.post("/favorites", authorizeUser, (req, res) => {
+  const favorite = { ...req.body, user_id: req.user_id };
   Favorite.create(favorite)
-    .then(data => {
-      res.status(201).json(data)
-    }).catch(console.error)
-})
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch(console.error);
+});
 
-app.put('/favorites/:id', authorizeUser, (req, res) => {
+app.put("/favorites/:id", authorizeUser, (req, res) => {
   Favorite.findByIdAndUpdate(
     req.params.id,
     req.body,
     { new: true },
     (err, favorite) => {
-      if (err) return res.status(500).send(err)
-      return res.send(favorite)
-    })
-})
+      if (err) return res.status(500).send(err);
+      return res.send(favorite);
+    }
+  );
+});
 
-app.delete('/favorites/:id', authorizeUser, (req, res) => {
-  Favorite.findByIdAndDelete(req.params.id)
-    .then(res.status(204).json({ message: 'Favorite deleted' }))
-})
+app.delete("/favorites/:id", authorizeUser, (req, res) => {
+  Favorite.findByIdAndDelete(req.params.id).then(
+    res.status(204).json({ message: "Favorite deleted" })
+  );
+});
 
-app.post('/register',
+app.post(
+  "/register",
   [
-    check('username', 'Please enter a valid username').not().isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'Password must be at least 6 characters').isLength({ min: 6 })
-  ], async (req, res) => {
+    check("username", "Please enter a valid username").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -78,7 +86,7 @@ app.post('/register',
       let user = await User.findOne({ username });
       if (user) {
         return res.status(400).json({
-          errors: ["User Already Exists"]
+          errors: ["User Already Exists"],
         });
       }
 
@@ -89,70 +97,75 @@ app.post('/register',
 
       await user.save();
 
-      const payload = { user_id: user.id }
+      const payload = { user_id: user.id };
 
-      jwt.sign(
-        payload,
-        process.env.JWT_SECRET,
-        (err, token) => {
-          if (err) throw err;
-          res.status(201).json({
-            token
-          });
-        }
-      );
+      jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+        if (err) throw err;
+        res.status(201).json({
+          token,
+        });
+      });
     } catch (err) {
       console.log(err.message);
       res.status(500).json({ errors: ["Error in Saving"] });
     }
-  });
+  }
+);
 
-app.post('/login',
+app.post(
+  "/login",
   [
-    check('username', 'Please enter a valid username').not().isEmpty(),
-    check('password', 'Password must be at least 6 characters').isLength({ min: 6 })
-  ], async (req, res) => {
-    const errors = validationResult(req)
+    check("username", "Please enter a valid username").not().isEmpty(),
+    check("password", "Password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.errors })
+      return res.status(400).json({ errors: errors.errors });
     }
-    const { username, password } = req.body
+    const { username, password } = req.body;
     try {
-      let user = await User.findOne({ username })
+      let user = await User.findOne({ username });
       if (!user) {
-        return res.status(400).json({ errors: ["Incorrect username or password"] })
+        return res
+          .status(400)
+          .json({ errors: ["Incorrect username or password"] });
       }
-      const isMatch = await bcrypt.compare(password, user.password)
+      const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res.status(400).json({ errors: ["Incorrect username or password"] })
+        return res
+          .status(400)
+          .json({ errors: ["Incorrect username or password"] });
       }
-      const payload = { user_id: user.id }
+      const payload = { user_id: user.id };
       jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
         if (err) throw err;
-        res.status(200).json({ token })
-      })
+        res.status(200).json({ token });
+      });
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ errors: ["Server Error"] })
+      console.error(err);
+      res.status(500).json({ errors: ["Server Error"] });
     }
-  })
+  }
+);
 
-app.post('/bug', (req, res) => {
-  Bugreport.create(req.body)
-    .then(data => res.status(201).json(data))
-})
+app.post("/bug", (req, res) => {
+  Bugreport.create(req.body).then((data) => res.status(201).json(data));
+});
 
 function authorizeUser(req, res, next) {
-  const token = req.header('Authorization')
+  const token = req.header("Authorization");
   if (!token) {
-    return res.status(401).json({ errors: ["Authorization error"] })
+    return res.status(401).json({ errors: ["Authorization error"] });
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user_id = decoded.user_id;
-    next()
+    next();
   } catch (err) {
-    console.error(err)
-    res.status(500).send({ errors: ['Invalid token'] })
+    console.error(err);
+    res.status(500).send({ errors: ["Invalid token"] });
   }
 }
